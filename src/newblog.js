e979a898251efbf2ea4f73ee83f4b4bc0261e118
aadhithiyan02 <104218446+aadhithiyan02@users.noreply.github.com>
2023-08-02 16:20:10 +0530

    Update newblog.js

--- src/newblog.js
+++ src/newblog.js
@@ -91,17 +91,119 @@
 
 
 
+// import { useState } from "react";
+// import { addDoc, collection } from 'firebase/firestore';
+// import { useHistory } from "react-router-dom";
+// import { db, auth, storage } from "./firebase";
+// import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+
+// const NewBlog = () => {
+//   const [title, setTitle] = useState('');
+//   const [content, setContent] = useState('');
+//   const [image, setImage] = useState(null);
+//   const blogCollection = collection(db, 'myblog');
+//   const history = useHistory();
+
+//   const handleImageChange = (e) => {
+//     const file = e.target.files[0];
+//     setImage(file);
+//   };
+
+//   const uploadImage = async () => {
+//     try {
+//       if (!image) return null; // No image selected
+//       const storageRef = ref(storage, `blog_images/${image.name}`);
+//       await uploadBytes(storageRef, image);
+//       const downloadURL = await getDownloadURL(storageRef);
+//       return downloadURL;
+//     } catch (error) {
+//       console.error('Error uploading image:', error);
+//       return null;
+//     }
+//   };
+
+//   const newBlog = async (e) => {
+//     e.preventDefault();
+//     try {
+//       let imageURL = null;
+
+//       if (image) {
+//         imageURL = await uploadImage();
+//       }
+
+//       const blogData = {
+//         title,
+//         content,
+//         author: {
+//           name: auth.currentUser.displayName,
+//           id: auth.currentUser.uid,
+//         },
+//       };
+
+//       // Add the imageURL field only if an image was uploaded
+//       if (imageURL) {
+//         blogData.imageURL = imageURL;
+//       }
+
+//       await addDoc(blogCollection, blogData);
+
+//       window.location.replace("/blogwebsite");
+
+//     } catch (error) {
+//       console.error('Error adding new blog:', error);
+//     }
+//   };
+
+//   return (
+//     <div className="create">
+//       <h2>Add New Blog</h2>
+//       <div className="container"></div>
+//       <form onSubmit={newBlog}>
+//         <div className="input">
+//           <label>Blog Title:</label>
+//           <input
+//             type="text"
+//             placeholder='Blog Title'
+//             value={title}
+//             onChange={(e) => setTitle(e.target.value)}
+//           />
+//         </div>
+//         <div className="input">
+//           <label>Blog Content:</label>
+//           <textarea
+//             name=""
+//             id=""
+//             cols="30"
+//             rows="10"
+//             placeholder='Enter Blog content'
+//             value={content}
+//             onChange={(e) => setContent(e.target.value)}
+//           ></textarea>
+//         </div>
+//         <div className='input'>
+//           <input type="file" onChange={handleImageChange} />
+//         </div>
+
+//         <button type="submit">Submit Blog</button>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default NewBlog;
+
+
 import { useState } from "react";
-import { addDoc, collection } from 'firebase/firestore';
+import { addDoc, collection } from "firebase/firestore";
 import { useHistory } from "react-router-dom";
 import { db, auth, storage } from "./firebase";
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
 const NewBlog = () => {
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
   const [image, setImage] = useState(null);
-  const blogCollection = collection(db, 'myblog');
+  const blogCollection = collection(db, "myblog");
   const history = useHistory();
 
   const handleImageChange = (e) => {
@@ -111,13 +213,13 @@ const NewBlog = () => {
 
   const uploadImage = async () => {
     try {
-      if (!image) return null; // No image selected
+      if (!image) return null;
       const storageRef = ref(storage, `blog_images/${image.name}`);
       await uploadBytes(storageRef, image);
       const downloadURL = await getDownloadURL(storageRef);
       return downloadURL;
     } catch (error) {
-      console.error('Error uploading image:', error);
+      console.error("Error uploading image:", error);
       return null;
     }
   };
@@ -140,17 +242,15 @@ const NewBlog = () => {
         },
       };
 
-      // Add the imageURL field only if an image was uploaded
       if (imageURL) {
         blogData.imageURL = imageURL;
       }
 
       await addDoc(blogCollection, blogData);
 
-      window.location.replace("/blogwebsite");
-
+      history.push("/blogwebsite");
     } catch (error) {
-      console.error('Error adding new blog:', error);
+      console.error("Error adding new blog:", error);
     }
   };
 
@@ -163,7 +263,7 @@ const NewBlog = () => {
           <label>Blog Title:</label>
           <input
             type="text"
-            placeholder='Blog Title'
+            placeholder="Blog Title"
             value={title}
             onChange={(e) => setTitle(e.target.value)}
           />
@@ -175,12 +275,12 @@ const NewBlog = () => {
             id=""
             cols="30"
             rows="10"
-            placeholder='Enter Blog content'
+            placeholder="Enter Blog content"
             value={content}
             onChange={(e) => setContent(e.target.value)}
           ></textarea>
         </div>
-        <div className='input'>
+        <div className="input">
           <input type="file" onChange={handleImageChange} />
         </div>
 
