c9f5770a7e6822da2771c02f7a9d6cebe363dc02
aadhithiyan02 <104218446+aadhithiyan02@users.noreply.github.com>
2023-08-02 16:18:49 +0530

    Update login.js

--- src/login.js
+++ src/login.js
@@ -1,31 +1,60 @@
-import {auth,provider} from "./firebase"
+// import {auth,provider} from "./firebase"
+// import { signInWithPopup } from "firebase/auth";
+// import { useHistory } from "react-router-dom";
+
+
+// const Login = ({setauth}) => {
+//     let history=useHistory();
+//     const signinwithgoogle = () => {
+//         signInWithPopup(auth,provider).then((result)=>{
+//             localStorage.setItem("auth",true);
+//             setauth(true);
+//             history.push("/blogwebsite");
+
+//         })
+//     }
+
+
+
+//     return (
+
+//         <div className="LoginPage">
+//             <p>Sign in with Google to continue</p>
+//             <button className="login-with-google-btn" onClick={signinwithgoogle}>
+//                 Click to sigin
+//             </button>
+//         </div>
+        
+//       );
+// }
+ 
+// export default Login;
+
+
+
+import { auth, provider } from "./firebase";
 import { signInWithPopup } from "firebase/auth";
 import { useHistory } from "react-router-dom";
 
+const Login = ({ setauth }) => {
+  let history = useHistory();
+  const signinwithgoogle = () => {
+    signInWithPopup(auth, provider).then((result) => {
+      localStorage.setItem("auth", true);
+      setauth(true);
+      history.push("/blogwebsite");
+    });
+  };
 
-const Login = ({setauth}) => {
-    let history=useHistory();
-    const signinwithgoogle = () => {
-        signInWithPopup(auth,provider).then((result)=>{
-            localStorage.setItem("auth",true);
-            setauth(true);
-            history.push("/blogwebsite");
+  return (
+    <div className="LoginPage">
+      <p>Sign in with Google to continue</p>
+      <button className="login-with-google-btn" onClick={signinwithgoogle}>
+        Click to sigin
+      </button>
+    </div>
+  );
+};
 
-        })
-    }
-
-
-
-    return (
-
-        <div className="LoginPage">
-            <p>Sign in with Google to continue</p>
-            <button className="login-with-google-btn" onClick={signinwithgoogle}>
-                Click to sigin
-            </button>
-        </div>
-        
-      );
-}
- 
 export default Login;
+
