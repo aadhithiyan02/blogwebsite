5fc24fd6516e4e94f33fd9c50b26f40733d718bb
aadhithiyan02 <104218446+aadhithiyan02@users.noreply.github.com>
2023-08-02 16:19:27 +0530

    Update navbar.js

--- src/navbar.js
+++ src/navbar.js
@@ -1,3 +1,54 @@
+// import { Link } from "react-router-dom";
+// import { getAuth, signOut } from "firebase/auth";
+// import { useHistory } from "react-router-dom";
+// import { useEffect } from "react";
+
+// const Navbar = ({ auth, setauth }) => {
+//   let history = useHistory();
+//   const authInstance = getAuth();
+
+//   const handleSignout = () => {
+//     signOut(authInstance)
+//       .then(() => {
+//         localStorage.clear();
+//         setauth(false);
+//         history.push("/Login");
+//       })
+//       .catch((error) => {
+//         console.error("Error signing out:", error);
+//       });
+//   };
+ 
+
+//   return (
+//     <nav className="navbar">
+//       <h1>Aadhi Blogs</h1>
+//       <div className="links">
+//         <Link to="/blogwebsite" className="create">
+//           Home
+//         </Link>
+        
+//         {!auth ? (
+//           <Link to="/Login" className="login">
+//             Login
+//           </Link>
+//         ) : (
+//           <>
+//           <Link to="/newblog" className="create">
+//           New Blog
+//         </Link>
+//           <button onClick={handleSignout}>Signout</button>
+//           </>
+          
+//         )}
+//       </div>
+//     </nav>
+//   );
+// };
+
+// export default Navbar;
+
+
 import { Link } from "react-router-dom";
 import { getAuth, signOut } from "firebase/auth";
 import { useHistory } from "react-router-dom";
@@ -18,7 +69,6 @@ const Navbar = ({ auth, setauth }) => {
         console.error("Error signing out:", error);
       });
   };
- 
 
   return (
     <nav className="navbar">
@@ -27,19 +77,18 @@ const Navbar = ({ auth, setauth }) => {
         <Link to="/blogwebsite" className="create">
           Home
         </Link>
-        
+
         {!auth ? (
           <Link to="/Login" className="login">
             Login
           </Link>
         ) : (
           <>
-          <Link to="/newblog" className="create">
-          New Blog
-        </Link>
-          <button onClick={handleSignout}>Signout</button>
+            <Link to="/newblog" className="create">
+              New Blog
+            </Link>
+            <button onClick={handleSignout}>Signout</button>
           </>
-          
         )}
       </div>
     </nav>
@@ -47,3 +96,4 @@ const Navbar = ({ auth, setauth }) => {
 };
 
 export default Navbar;
+
